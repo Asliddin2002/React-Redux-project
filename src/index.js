a15e95a96f053b2c4c63518c5a5dde11074708b1
Asliddin2002 <wiut00012247@gmail.com>
2022-04-26 23:04:11 +0500

    edit2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React,{Suspense} from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {createStore} from 'redux'
@@ -9,14 +9,17 @@ import {reducer} from './Redux/Reducer'
 const store = createStore(reducer)
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Suspense fallback={(<div className="text-center">Loading ...</div>)}>
+        <Provider store={store}>
     <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Suspense>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
